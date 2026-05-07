11166e2373928f139c13aa96d3393c9eb180d6ad
G Sai Charan Teja <gavidisaicharanteja@gmail.com>
2026-05-07 16:20:14 +0530

    Update CustomUserDetailsService.java

--- src/main/java/com/example/StudentManagementSystem/StudentManagementSystem/security/CustomUserDetailsService.java
+++ src/main/java/com/example/StudentManagementSystem/StudentManagementSystem/security/CustomUserDetailsService.java
@@ -17,11 +17,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
 
-        User user = userRepository.findByUsername(username);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getUsername())
